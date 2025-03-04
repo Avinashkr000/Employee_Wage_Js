5ed8a3becc8ff46c739f9d9003056a19b763ee8e
Avinashkr000 <akum97543@gmail.com>
2025-03-04 12:09:02 +0530

    [Avinash kumar] calculate wages for a month with condition check

--- src/employeeWage.js
+++ src/employeeWage.js
@@ -3,7 +3,8 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-const NUM_OF_WORKING_DAYS = 20; 
+const MAX_HRS_IN_MONTH = 160;
+const NUM_OF_WORKING_DAYS = 20;
 
 function getWorkingHours(empCheck) {
     switch (empCheck) {
@@ -16,14 +17,16 @@ function getWorkingHours(empCheck) {
     }
 }
 
-let empHrs = 0;
-for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+
+
+while (totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
-    empHrs += getWorkingHours(empCheck);
+    totalEmpHrs += getWorkingHours(empCheck);
 }
 
+let empWage = totalEmpHrs * WAGE_PER_HOUR;
 
-let empWage = empHrs * WAGE_PER_HOUR;
-
-
-console.log("Total Hours: " + empHrs + " | Employee Wage for the Month: " + empWage + " $");
+console.log("Total Days: " + totalWorkingDays + " | Total Hrs: " + totalEmpHrs + " | Emp Wage: " + empWage);
