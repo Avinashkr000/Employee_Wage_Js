125c0e17351172ac317ecafb80f52ca9e2cd904f
Avinashkr000 <akum97543@gmail.com>
2025-03-04 11:59:28 +0530

    [Avinash kumar] Calculating Wages for

--- src/employeeWage.js
+++ src/employeeWage.js
@@ -3,6 +3,7 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20; 
 
 function getWorkingHours(empCheck) {
     switch (empCheck) {
@@ -15,9 +16,14 @@ function getWorkingHours(empCheck) {
     }
 }
 
-let empCheck = Math.floor(Math.random() * 10) % 3; 
-let empHrs = getWorkingHours(empCheck);
+let empHrs = 0;
+for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs += getWorkingHours(empCheck);
+}
+
+
 let empWage = empHrs * WAGE_PER_HOUR;
 
-console.log("Employee Work Hours: " + empHrs);
-console.log("Employee Wage: " + empWage);
+
+console.log("Total Hours: " + empHrs + " | Employee Wage for the Month: " + empWage + " $");
