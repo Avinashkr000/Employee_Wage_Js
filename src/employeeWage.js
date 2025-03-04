78597d4b458de9af5b7b6314aa546d94840efd44
Avinashkr000 <akum97543@gmail.com>
2025-03-04 11:38:30 +0530

    [Avinash kumar] Calculate Employee Wage

--- src/employeeWage.js
+++ src/employeeWage.js
@@ -1,2 +1,26 @@
-const IS_PRESENT = Math.random() < 0.5 ? "Absent" : "Present"; 
-console.log(`Employee is ${IS_PRESENT}`);
+const IS_ABSENT = 0;
+const PART_TIME = 1;
+const FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+
+let empCheck = Math.floor(Math.random() * 3);
+let empHours = 0;
+
+switch (empCheck) {
+    case IS_ABSENT:
+        empHours = 0;
+        break;
+    case PART_TIME:
+        empHours = PART_TIME_HOURS;
+        break;
+    case FULL_TIME:
+        empHours = FULL_TIME_HOURS;
+        break;
+    default:
+        empHours = 0;
+}
+
+let empWage = empHours * WAGE_PER_HOUR;
+console.log(`Employee worked ${empHours} hours and earned $${empWage}`);
