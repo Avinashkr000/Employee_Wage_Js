ec71433b8ac15b61ce6eb4e7bf353dc4974eb1d4
Avinashkr000 <akum97543@gmail.com>
2025-03-04 12:16:21 +0530

    [Avinash kumar] store daily wage allong with total wage

--- src/employeeWage.js
+++ src/employeeWage.js
@@ -1,3 +1,7 @@
+function calcDailyWage(empHrs) {
+    return empHrs * WAGE_PER_HOUR;
+}
+
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
@@ -17,16 +21,25 @@ function getWorkingHours(empCheck) {
     }
 }
 
+
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
+let empDailyWageArr = new Array(); 
 
 
 while (totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
     totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
-    totalEmpHrs += getWorkingHours(empCheck);
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    empDailyWageArr.push(calcDailyWage(empHrs)); 
 }
 
-let empWage = totalEmpHrs * WAGE_PER_HOUR;
 
-console.log("Total Days: " + totalWorkingDays + " | Total Hrs: " + totalEmpHrs + " | Emp Wage: " + empWage);
+let empWage = calcDailyWage(totalEmpHrs);
+
+
+console.log(" Total Days: " + totalWorkingDays +
+            " | Total Hrs: " + totalEmpHrs +
+            " | Emp Wage: " + empWage);
+console.log("Daily Wages Array: " + empDailyWageArr);
